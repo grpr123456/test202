e6d0e4da7a1856cdf61ec5eace885d2c9a2edba2
grpr123456 <grpr.mca21@gmail.com>
2024-08-05 03:57:36 +0530

    third

--- main.tf
+++ main.tf
@@ -5,8 +5,8 @@ provider "aws" {
 
 module "first" {
     source = "git::https://github.com/grpr123456/test202.git"
-    vpc_cidr="10.1.0.0/16"
-    subnet1_cidr="10.1.1.0/24"
-    subnet2_cidr="10.1.2.0/24"
+    vpc_cidr="10.2.0.0/16"
+    subnet1_cidr="10.2.1.0/24"
+    subnet2_cidr="10.2.2.0/24"
   
 }
